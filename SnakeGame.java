d205eb2936428acb88c60887176c84e57e068ecc
ecaterina <sohkil14@gmail.com>
2023-11-02 15:19:40 +0200

    init commit

---
+++ SnakeGame.java
@@ -0,0 +1,95 @@
+package com.javarush.games.snake;
+
+import com.javarush.engine.cell.*;
+
+public class SnakeGame extends Game {
+    public static final int WIDTH = 15;
+    public static final int HEIGHT = 15;
+    private Snake snake;
+    private int turnDelay;
+    private Apple apple;
+    private boolean isGameStopped;
+    private static final int GOAL = 28;
+    private  int score;
+
+
+    @Override
+    public void initialize() {
+        setScreenSize(WIDTH, HEIGHT);
+        createGame();
+    }
+    private void createGame(){
+        snake = new Snake(WIDTH / 2, HEIGHT / 2);
+        createNewApple();
+        isGameStopped = false;
+        drawScene();
+        turnDelay = 300;
+        setTurnTimer(turnDelay);
+        score = 0;
+        setScore(score);
+    }
+    private void drawScene(){
+        for(int x = 0; x < WIDTH; x++){
+            for(int y = 0; y < HEIGHT; y++){
+                setCellValueEx(x,y,Color.DARKSEAGREEN,"");
+            }
+        }
+        snake.draw(this);
+        apple.draw(this);
+    }
+
+    @Override
+    public void onTurn(int step) {
+        snake.move(apple);
+        if(!apple.isAlive){
+            createNewApple();
+            score += 5;
+            setScore(score);
+            turnDelay -= 10;
+            setTurnTimer(turnDelay);
+        }
+        if(!snake.isAlive) {
+            gameOver();
+        }
+        if(snake.getLength() > GOAL){
+            win();
+        }
+        drawScene();
+
+    }
+
+    @Override
+    public void onKeyPress(Key key) {
+        if(key == Key.SPACE && isGameStopped){
+            createGame();
+        }
+        if(key == Key.LEFT){
+            snake.setDirection(Direction.LEFT);
+        }else if(key == Key.RIGHT){
+            snake.setDirection(Direction.RIGHT);
+        }else if(key == Key.UP){
+            snake.setDirection(Direction.UP);
+        }else if(key == Key.DOWN){
+            snake.setDirection(Direction.DOWN);
+        }
+    }
+    private void createNewApple(){
+        Apple newApple;
+        do {
+            int x = getRandomNumber(WIDTH);
+            int y = getRandomNumber(HEIGHT);
+            newApple = new Apple(x, y);
+        }while (snake.checkCollision(newApple));
+        apple = newApple;
+    }
+    private void gameOver(){
+        stopTurnTimer();
+        isGameStopped = true;
+        showMessageDialog(Color.WHITE, "GAME OVER", Color.BLACK, 50);
+    }
+    private void win(){
+        stopTurnTimer();
+        isGameStopped = true;
+        showMessageDialog(Color.CORAL, "YOU WIN!", Color.BLACK, 50);
+    }
+}
